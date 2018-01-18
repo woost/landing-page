1940c9c1816029ff7116fc5b5132c331df53426f
johannes karoff <johannes@karoff.net>
2018-01-18 13:06:49 +0100

    add team section and use bootstrap sass

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task('copy', function() {
       '!**/bootstrap-theme.*',
       '!**/*.map'
     ])
-    .pipe(gulp.dest('vendor/bootstrap'))
+    .pipe(gulp.dest('vendor/bootstrap/js'))
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
     .pipe(gulp.dest('vendor/jquery'))
