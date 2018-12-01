bd95973c06ceec7559c2a4380baa7c03c2315d15
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-12-01 18:01:54 +0100

    Fix deploy?

--- docker_publish.sh
+++ docker_publish.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -e
+set -o pipefail
+
+[[ -n "$DOCKER_USERNAME" ]] || exit 0
+[[ -n "$DOCKER_PASSWORD" ]] || exit 0
+[[ -n "$DOCKER_REGISTRY" ]] || exit 0
 
 [[ "$TRAVIS_BRANCH" == "master" ]] || exit 0
 
 docker images
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" "$DOCKER_REGISTRY" --password-stdin;
 
-docker push woost/landing
+docker push "$DOCKER_REGISTRY/woost/landing"
 
 [[ -n "$STAGING_URL" ]] || exit 0
 
+echo "Triggering landing page deploy..."
 curl $STAGING_URL 2> /dev/null
