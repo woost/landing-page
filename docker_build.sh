a5a4b3ea3bf4de803f97ffafc8fff9204cce802b
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-12-02 20:50:13 +0100

    reactivate docker cache

--- docker_build.sh
+++ docker_build.sh
@@ -6,6 +6,5 @@ grep min.css index.html
 
 git_sha="$(git rev-parse --short HEAD)"
 
-# docker pull woost/landing:latest
-docker build --tag woost/landing:$git_sha --tag woost/landing:latest .
-# docker build --cache-from woost/landing:latest --tag woost/landing:$git_sha --tag woost/landing:latest .
+docker pull woost/landing:latest
+docker build --cache-from woost/landing:latest --tag woost/landing:$git_sha --tag woost/landing:latest .
