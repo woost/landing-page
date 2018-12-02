7acfc910cc6d4cc3e8d56a8bec87ae1a653fabdb
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-12-02 18:44:00 +0100

    disable more docker caches

--- docker_build.sh
+++ docker_build.sh
@@ -5,6 +5,6 @@
 
 git_sha="$(git rev-parse --short HEAD)"
 
-docker pull woost/landing:latest
+# docker pull woost/landing:latest
 docker build --tag woost/landing:$git_sha --tag woost/landing:latest .
 # docker build --cache-from woost/landing:latest --tag woost/landing:$git_sha --tag woost/landing:latest .
